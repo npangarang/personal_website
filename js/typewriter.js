4dfaee3d325a67e42cd295b55eaace9297afa372
Neel_Panging <Neel.Panging@Dell.com>
2023-05-04 12:17:09 -0500

    hello bubble!

--- js/typewriter.js
+++ js/typewriter.js
@@ -40,4 +40,55 @@ function typeWriter() {
   setTimeout(typeWriter, wait);
 }
 
-document.addEventListener('DOMContentLoaded', typeWriter);
+function typeWriterChatBubble() {
+  const chatBubble = document.getElementById('chat-bubble');
+  const messages = [
+    'Hello', 'Bonjour', 'Hola', 'こんにちは', '안녕하세요', 'Ciao',
+    'Hallo', 'Привет', '你好', 'سلام', 'שלום', 'नमस्ते',
+    'สวัสดี', 'Hej', 'Merhaba', 'Olá', 'Γεια σας', 'Selamat'
+  ];
+  let currentMessage = 0;
+  let textIndex = 0;
+  let currentText = '';
+  let isDeleting = false;
+  let wait = 2000;
+
+  function updateChatBubble() {
+    if (isDeleting) {
+      currentText = messages[currentMessage].substring(0, textIndex);
+      textIndex--;
+    } else {
+      currentText = messages[currentMessage].substring(0, textIndex);
+      textIndex++;
+    }
+
+    chatBubble.textContent = currentText;
+
+    if (textIndex === messages[currentMessage].length + 1 && !isDeleting) {
+      isDeleting = true;
+      wait = 2000;
+    } else if (textIndex === 0 && isDeleting) {
+      isDeleting = false;
+      currentMessage = (currentMessage + 1) % messages.length;
+      wait = 0;
+    } else {
+      wait = isDeleting ? 25 : 50;
+    }
+
+    setTimeout(updateChatBubble, wait);
+  }
+
+  updateChatBubble();
+}
+
+
+// document.addEventListener('DOMContentLoaded', typeWriter);
+
+document.addEventListener('DOMContentLoaded', function() {
+  typeWriter();
+  typeWriterChatBubble();
+});
+
+
+
+

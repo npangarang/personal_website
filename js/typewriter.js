d9d36fc3e66cb68981fac002e0f4975255ab4b77
Neel_Panging <Neel.Panging@Dell.com>
2023-05-04 12:45:56 -0500

    added company/school logos

--- js/typewriter.js
+++ js/typewriter.js
@@ -87,6 +87,19 @@ function typeWriterChatBubble() {
 document.addEventListener('DOMContentLoaded', function() {
   typeWriter();
   typeWriterChatBubble();
+
+  const badges = document.querySelectorAll('.badge');
+  const colors = [
+    '#F44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5', '#2196F3',
+    '#03A9F4', '#00BCD4', '#009688', '#4CAF50', '#8BC34A', '#CDDC39',
+    '#FFC107', '#FF9800', '#FF5722'
+  ];
+
+    badges.forEach(function(badge) {
+      const randomColor = colors[Math.floor(Math.random() * colors.length)];
+      badge.style.backgroundColor = randomColor;
+    });
+
 });
 
 
